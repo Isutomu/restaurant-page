63f3e39e0ba82db3042b7ec977dbf62f68604912
Isutomu <isutomu@gmail.com>
2024-05-11 14:54:35 -0300

    Create function to generate 'menu' tab html

--- src/script/menu.js
+++ src/script/menu.js
@@ -1,3 +1,31 @@
-function dishCard(name, description, imgPath) {
-    
-}
+import dishesData from '../assets/dishes.json';
+
+function createDishCard(dishInfo) {
+    const card = document.createElement('div');
+    card.classList.add('dish-card');
+
+    const img = document.createElement('img');
+    img.src = dishInfo.image;
+    card.appendChild(img);
+
+    const nameElem = document.createElement('h2');
+    nameElem.textContent = dishInfo.name;
+    card.appendChild(nameElem);
+
+    const descElem = document.createElement('span');
+    descElem.textContent = dishInfo.description;
+    card.appendChild(descElem);
+
+    return card;
+};
+
+export default function () {
+    const container = document.createElement('.container');
+    container.classList.add('menu');
+
+    dishesData.map(dishInfo => {
+        container.appendChild(createDishCard(dishInfo))
+    });
+
+    return container;
+};
