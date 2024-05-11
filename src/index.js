eb3e97c7c8c1f5090c0d13fac1a864d62e1a2bfd
Isutomu <isutomu@gmail.com>
2024-05-10 22:52:14 -0300

    Add imports of all js and css to bundler input (index.jss)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-alert('lol');
+import './style/css-reset.css';
+import './style/css-home.css';
+import './style/css-menu.css';
+import './style/css-contact.css';
+
+import home from './script/home';
+import menu from './script/menu';
+import contact from './script/contact';
+
+
